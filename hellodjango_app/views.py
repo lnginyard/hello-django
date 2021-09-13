a76c7ffad46a24ad3d2790da02291a5018d28d73
Lorenzo Ginyard <lnginyard@gmail.kenzie.academy.com>
2021-09-12 21:16:50 -0400

    "create django app to display hello django! on web page rendered from template view with template tags and date & time page rendered with iported date and time module."

---
+++ hellodjango_app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    now = datetime.now()
+
+    return render(
+        request,
+        "icndex_view.html",
+        # Relative path from the 'templates'\
+        # folder to the template file
+        # "index.html", # Use this code for VS 2017 15.7 and earlier
+        {
+            'title': "Hello Django",
+            'message': "Hello Django!",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
